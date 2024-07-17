4d44497014976918c42862f64951bfd6be44c630
Elhan23 <elhan13chik@gmail.com>
2024-07-17 23:30:34 +0600

    changes file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import Layout from './layout/Layout'
+import React from "react";
+import Layout from "./layout/Layout";
+import Product from "./components/Product/Product";
 
 function App() {
   return (
     <div>
-       <Layout>
-          
-       </Layout>
+      <Layout/>
+      <Product />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './scss/index.scss'
+import React from "react";
+import ReactDOM from "react-dom/client"; 
+import App from "./App.jsx";
+import { ApolloProvider } from "@apollo/client";
+import client from "./apolloClient/apolloClient.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "./scss/index.scss";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
+);
